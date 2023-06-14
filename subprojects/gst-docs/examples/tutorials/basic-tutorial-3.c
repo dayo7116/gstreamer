d7dfbc05c925c9f01b91b6dd4d334f2d23759f45
dayongli <dayong.li@mthreads.com>
2023-06-14 19:40:57 +0800

    --other=0 amzon demo wasapisrc替换成audiotestsrc可以抓取音频帧

--- subprojects/gst-docs/examples/tutorials/basic-tutorial-3.c
+++ subprojects/gst-docs/examples/tutorials/basic-tutorial-3.c
@@ -9,6 +9,10 @@
 
 #define CAPS "video/x-raw,format=RGB,width=160,pixel-aspect-ratio=1/1"
 
+static void eos_cb(GstElement* sink, gpointer data) {
+  g_print("eos_cb \n");
+}
+
 // 回调函数，将音频数据填充到缓冲区中
 static GstFlowReturn on_new_sample_from_sink(GstElement* sink, gpointer data) {
   GstSample* sample = gst_app_sink_pull_sample(GST_APP_SINK(sink));
@@ -36,12 +40,12 @@ static GstFlowReturn on_new_sample_from_sink(GstElement* sink, gpointer data) {
     s = gst_caps_get_structure(caps, 0);
 
     /* we need to get the final caps on the buffer to get the size */
-    gboolean res = gst_structure_get_int(s, "width", &width);
+    /*gboolean res = gst_structure_get_int(s, "width", &width);
     res |= gst_structure_get_int(s, "height", &height);
     if (!res) {
       g_print("could not get snapshot dimension\n");
       exit(-1);
-    }
+    }*/
 
     /* create pixmap from buffer and save, gstreamer video buffers have a stride
      * that is rounded up to the nearest multiple of 4 */
@@ -78,9 +82,13 @@ static gboolean bus_call(GstBus* bus, GstMessage* msg, gpointer data) {
   case GST_MESSAGE_ERROR: {
     gchar* debug;
     GError* err;
+    G_FILE_ERROR;
+    G_FILE_ERROR_NOENT;
+
     gst_message_parse_error(msg, &err, &debug);
-    g_print("Error: %s\n", err->message);
-    g_error_free(err);
+    g_print("Error: %s, domain:%s, code:%d\n", err->message, g_quark_to_string(err->domain), err->code);
+    g_print("Debug: %s\n", debug);
+     g_error_free(err);
     g_free(debug);
     g_main_loop_quit(loop);
     break;
@@ -96,8 +104,6 @@ static gboolean bus_call(GstBus* bus, GstMessage* msg, gpointer data) {
 int
 main(int argc, char* argv[])
 {
-  GstElement* pipeline, * sink;
-  gint width, height;
   GstSample* sample;
   gchar* descr;
   GError* error = NULL;
@@ -114,46 +120,67 @@ main(int argc, char* argv[])
     //exit(-1);
   }
 
-  /* create a new pipeline */
-  descr =
-    g_strdup_printf("uridecodebin uri=%s ! videoconvert ! videoscale ! "
-      " appsink name=sink caps=\"" CAPS "\"", "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm");
-  pipeline = gst_parse_launch(descr, &error);
-  
-  if (error != NULL) {
-    g_print("could not construct pipeline: %s\n", error->message);
-    g_clear_error(&error);
-    exit(-1);
-  }
+  GstElement * appsink_audio, * audio_queue, * pipeline, * audio_filter;
+  GstCaps* caps;
 
-  /* get sink */
-  sink = gst_bin_get_by_name(GST_BIN(pipeline), "sink");
+  audio_filter = gst_element_factory_make("capsfilter", "audio_filter");
+  //caps = gst_caps_from_string("audio/mpeg, stream-format=(string) raw");
+  caps = gst_caps_new_any();
+  g_object_set(G_OBJECT(audio_filter), "caps", caps, NULL);
+  gst_caps_unref(caps);
 
-  GstAppSinkCallbacks callbacks;
-  // 设置回调函数，以处理从音频捕获元素捕获的音频数据
-  memset(&callbacks, 0, sizeof(callbacks));
-  callbacks.new_sample = on_new_sample_from_sink;
-  gst_app_sink_set_callbacks(GST_APP_SINK(sink), &callbacks, NULL, NULL);
+  appsink_audio = gst_element_factory_make("appsink", "appsink_audio");
 
-  // 启动 GStreamer 管道
-  ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+  g_object_set(G_OBJECT(appsink_audio), "emit-signals", TRUE, "sync", FALSE, NULL);
+  g_signal_connect(appsink_audio, "new-sample", G_CALLBACK(on_new_sample_from_sink), NULL);
+  g_signal_connect(appsink_audio, "eos", G_CALLBACK(eos_cb), NULL);
 
-  if (ret == GST_STATE_CHANGE_FAILURE) {
-    g_print("Unable to set pipeline to playing state.\n");
+  audio_queue = gst_element_factory_make("queue", "audio_queue");
+  pipeline = gst_pipeline_new("audio-capture-pipeline");
+
+  GstElement* audiosrc, * audioconvert, * aac_enc, * audio_resample, * audio_src_filter, * aac_parse;
+  aac_parse = gst_element_factory_make("aacparse", "aac_parse");
+  audiosrc = gst_element_factory_make("audiotestsrc", "audiosrc");//wasapisrc
+  //g_object_set(G_OBJECT(audiosrc), "device", "{0.0.1.00000000}.{59abac3a - 90f8 - 4ee1 - 9e4a - 4e34289043d4}", "low-latency", TRUE, "use-audioclient3", TRUE, NULL);
+
+  audio_src_filter = gst_element_factory_make("capsfilter", "audio_src_filter");
+  //caps = gst_caps_from_string("audio/x-raw, rate=(int) { 16000, 24000, 48000 }");
+  caps = gst_caps_new_any();
+  g_object_set(G_OBJECT(audio_src_filter), "caps", caps, NULL);
+  gst_caps_unref(caps);
+
+  audioconvert = gst_element_factory_make("audioconvert", "audioconvert");
+  aac_enc = gst_element_factory_make("avenc_aac", "aac_enc");
+  audio_resample = gst_element_factory_make("audioresample", "audioresample");
+
+  gst_bin_add_many(GST_BIN(pipeline),appsink_audio, audiosrc, audioconvert, aac_enc,
+    audio_queue, audio_filter, audio_resample, audio_src_filter, aac_parse, NULL);
+  if (!gst_element_link_many(audiosrc, audio_resample, audio_queue, audioconvert, audio_src_filter, aac_enc, aac_parse, audio_filter,
+    appsink_audio,
+    NULL)) {
+    g_printerr("Audio elements could not be linked.\n");
     gst_object_unref(pipeline);
     return -1;
   }
 
-  // 获取 GStreamer 消息总线
-  GstBus* bus = gst_element_get_bus(pipeline);
   GMainLoop* loop = g_main_loop_new(NULL, FALSE);
-  // 开始 GStreamer 循环
-  gst_bus_add_watch(bus, bus_call, loop);
+
+  /* Instruct the bus to emit signals for each received message, and connect to the interesting signals */
+  GstBus* bus = gst_element_get_bus(pipeline);
+  gst_bus_add_signal_watch(bus);
+  g_signal_connect(G_OBJECT(bus), "message::error", (GCallback)bus_call, loop);
+  gst_object_unref(bus);
+
+  ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+  if (ret == GST_STATE_CHANGE_FAILURE) {
+    g_printerr("Unable to set the pipeline to the playing state.\n");
+    return -1;
+  }
+  
   g_main_loop_run(loop);
 
   /* cleanup and exit */
   gst_element_set_state(pipeline, GST_STATE_NULL);
-  gst_object_unref(sink);
   gst_object_unref(pipeline);
 
   exit(0);
